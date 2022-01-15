4f88960331360c15c54a704743a21e119326277e
Daehoon <foreat_14@naver.com>
2022-01-15 14:11:53 +0900

    init

---
+++ src/main/kotlin/dh/autotrading/autotrading/AutotradingApplication.kt
@@ -0,0 +1,11 @@
+package dh.autotrading.autotrading
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class AutotradingApplication
+
+fun main(args: Array<String>) {
+    runApplication<AutotradingApplication>(*args)
+}
---
+++ src/test/kotlin/dh/autotrading/autotrading/AutotradingApplicationTests.kt
@@ -0,0 +1,13 @@
+package dh.autotrading.autotrading
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class AutotradingApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
